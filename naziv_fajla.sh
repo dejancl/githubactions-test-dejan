80a403b6f460773fd4550543a341b4469e4d8a70
dejancl <dejan.andjelkovic@cyberlab.rs>
2023-03-09 13:49:58 +0100

    123

--- naziv_fajla.sh
+++ naziv_fajla.sh
@@ -1,13 +1,12 @@
 #! bin/bash
 
 FILE="$1"
-if [ -d "$1" ]
+if [ -d "$FILE" ]
 then
     # The /my-dir directory exists, so print a message
     echo "The /my-dir directory exists."
 else
     # The /my-dir directory does not exist, so print a message and create it
     echo "The /my-dir directory does not exist. Creating it now..."
-    touch $1
+    touch $FILE
 fi
-cat $2 >> $1
